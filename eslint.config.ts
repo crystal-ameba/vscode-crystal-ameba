36deb9063a1e9bfef06e8aa7edd6f53346af59af
Sijawusz Pur Rahnama <sija@sija.pl>
2026-08-13 00:42:25 +0200

    Replace TSLint with ESLint

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+]);
