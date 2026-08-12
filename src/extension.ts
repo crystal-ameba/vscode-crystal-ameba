2f2d9a9e3b43139c298f0128f49716a856c737ba
Sijawusz Pur Rahnama <sija@sija.pl>
2026-08-12 23:15:48 +0200

    Reformat codebase

--- src/extension.ts
+++ src/extension.ts
@@ -1,234 +1,285 @@
 import {
-    commands, ExtensionContext, languages,
-    OutputChannel,
-    TextDocument, Uri, window,
-    workspace, WorkspaceFolder
+  commands,
+  ExtensionContext,
+  languages,
+  OutputChannel,
+  TextDocument,
+  Uri,
+  window,
+  workspace,
+  WorkspaceFolder,
 } from 'vscode';
-import * as path from 'path'
+import * as path from 'path';
 
 import { Ameba } from './ameba';
 import { getConfig, LintScope, LintTrigger } from './configuration';
 
-
 export let outputChannel: OutputChannel;
 
 export function activate(context: ExtensionContext) {
-    outputChannel = window.createOutputChannel("Crystal Ameba", "log");
-    context.subscriptions.push(outputChannel);
+  outputChannel = window.createOutputChannel('Crystal Ameba', 'log');
+  context.subscriptions.push(outputChannel);
 
-    const diag = languages.createDiagnosticCollection('crystal');
-    let ameba: Ameba | null = new Ameba(diag);
+  const diag = languages.createDiagnosticCollection('crystal');
+  let ameba: Ameba | null = new Ameba(diag);
 
-    context.subscriptions.push(diag);
+  context.subscriptions.push(diag);
 
-    context.subscriptions.push(
-        commands.registerCommand('crystal.ameba.lint', () => {
-            if (ameba) {
-                const editor = window.activeTextEditor;
-                if (editor) {
-                    outputChannel.appendLine('[Lint] Running ameba on current document')
-                    ameba.execute(editor.document);
-                }
-            } else {
-                window.showWarningMessage(
-                    'Ameba has been disabled for this workspace.',
-                    'Enable'
-                ).then(
-                    enable => {
-                        if (!enable) return;
-                        ameba = new Ameba(diag);
-                        const editor = window.activeTextEditor;
-                        if (editor) {
-                            outputChannel.appendLine('[Enable] Running ameba on current document')
-                            ameba.execute(editor.document);
-                        }
-                    },
-                    _ => { }
+  context.subscriptions.push(
+    commands.registerCommand('crystal.ameba.lint', () => {
+      if (ameba) {
+        const editor = window.activeTextEditor;
+        if (editor) {
+          outputChannel.appendLine('[Lint] Running ameba on current document');
+          ameba.execute(editor.document);
+        }
+      } else {
+        window
+          .showWarningMessage(
+            'Ameba has been disabled for this workspace.',
+            'Enable'
+          )
+          .then(
+            (enable) => {
+              if (!enable) return;
+              ameba = new Ameba(diag);
+              const editor = window.activeTextEditor;
+              if (editor) {
+                outputChannel.appendLine(
+                  '[Enable] Running ameba on current document'
                 );
-            }
-        })
-    );
+                ameba.execute(editor.document);
+              }
+            },
+            (_) => {}
+          );
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        commands.registerCommand('crystal.ameba.lint-workspace', () => {
-            if (ameba) {
-                outputChannel.appendLine('[Lint] Running ameba on current workspace')
-                executeAmebaOnWorkspace(ameba)
-            } else {
-                window.showWarningMessage(
-                    'Ameba has been disabled for this workspace.',
-                    'Enable'
-                ).then(
-                    enable => {
-                        if (!enable) return;
-                        ameba = new Ameba(diag);
-                        executeAmebaOnWorkspace(ameba)
-                    },
-                    _ => { }
-                );
-            }
-        })
-    )
+  context.subscriptions.push(
+    commands.registerCommand('crystal.ameba.lint-workspace', () => {
+      if (ameba) {
+        outputChannel.appendLine('[Lint] Running ameba on current workspace');
+        executeAmebaOnWorkspace(ameba);
+      } else {
+        window
+          .showWarningMessage(
+            'Ameba has been disabled for this workspace.',
+            'Enable'
+          )
+          .then(
+            (enable) => {
+              if (!enable) return;
+              ameba = new Ameba(diag);
+              executeAmebaOnWorkspace(ameba);
+            },
+            (_) => {}
+          );
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        commands.registerCommand('crystal.ameba.restart', () => {
-            if (ameba) {
-                const editor = window.activeTextEditor;
-                if (editor) {
-                    outputChannel.appendLine(`[Restart] Clearing diagnostics for ${getRelativePath(editor.document)}`)
-                    ameba.clear(editor.document.uri);
-                }
-            } else {
-                outputChannel.appendLine('[Restart] Starting ameba')
-                ameba = new Ameba(diag);
-                executeAmebaOnWorkspace(ameba);
-            }
-        })
-    );
+  context.subscriptions.push(
+    commands.registerCommand('crystal.ameba.restart', () => {
+      if (ameba) {
+        const editor = window.activeTextEditor;
+        if (editor) {
+          outputChannel.appendLine(
+            `[Restart] Clearing diagnostics for ${getRelativePath(editor.document)}`
+          );
+          ameba.clear(editor.document.uri);
+        }
+      } else {
+        outputChannel.appendLine('[Restart] Starting ameba');
+        ameba = new Ameba(diag);
+        executeAmebaOnWorkspace(ameba);
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        commands.registerCommand('crystal.ameba.disable', () => {
-            if (!ameba) return;
-            outputChannel.appendLine('[Disable] Disabling ameba for this session')
-            ameba.clear();
-            ameba = null;
-        })
-    );
+  context.subscriptions.push(
+    commands.registerCommand('crystal.ameba.disable', () => {
+      if (!ameba) return;
+      outputChannel.appendLine('[Disable] Disabling ameba for this session');
+      ameba.clear();
+      ameba = null;
+    })
+  );
 
-    context.subscriptions.push(
-        workspace.onDidChangeConfiguration(_ => {
-            if (!ameba) return;
-            outputChannel.appendLine(`[Config] Reloading diagnostics after config change`)
-            ameba.config = getConfig();
-            ameba.clear()
-            executeAmebaOnWorkspace(ameba)
-        })
-    );
+  context.subscriptions.push(
+    workspace.onDidChangeConfiguration((_) => {
+      if (!ameba) return;
+      outputChannel.appendLine(
+        `[Config] Reloading diagnostics after config change`
+      );
+      ameba.config = getConfig();
+      ameba.clear();
+      executeAmebaOnWorkspace(ameba);
+    })
+  );
 
-    executeAmebaOnWorkspace(ameba);
+  executeAmebaOnWorkspace(ameba);
 
-    // This can happen when a file is open _or_ when a file's language id changes
-    context.subscriptions.push(
-        workspace.onDidOpenTextDocument(doc => {
-            if (ameba && ameba.config.trigger !== LintTrigger.None && isValidCrystalDocument(doc)) {
-                if (isDocumentVirtual(doc)) {
-                    if (ameba.config.trigger === LintTrigger.Type) {
-                        outputChannel.appendLine(`[Open] Running ameba on ${getRelativePath(doc)}`);
-                        ameba.execute(doc, true);
-                    }
-                } else {
-                    outputChannel.appendLine(`[Open] Running ameba on ${getRelativePath(doc)}`);
-                    ameba.execute(doc);
-                }
-            }
-        })
-    );
+  // This can happen when a file is open _or_ when a file's language id changes
+  context.subscriptions.push(
+    workspace.onDidOpenTextDocument((doc) => {
+      if (
+        ameba &&
+        ameba.config.trigger !== LintTrigger.None &&
+        isValidCrystalDocument(doc)
+      ) {
+        if (isDocumentVirtual(doc)) {
+          if (ameba.config.trigger === LintTrigger.Type) {
+            outputChannel.appendLine(
+              `[Open] Running ameba on ${getRelativePath(doc)}`
+            );
+            ameba.execute(doc, true);
+          }
+        } else {
+          outputChannel.appendLine(
+            `[Open] Running ameba on ${getRelativePath(doc)}`
+          );
+          ameba.execute(doc);
+        }
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        workspace.onDidChangeTextDocument(e => {
-            if (ameba && ameba.config.trigger === LintTrigger.Type && isValidCrystalDocument(e.document)) {
-                outputChannel.appendLine(`[Change] Running ameba on ${getRelativePath(e.document)}`);
-                ameba.execute(e.document, isDocumentVirtual(e.document));
-            }
-        })
-    );
+  context.subscriptions.push(
+    workspace.onDidChangeTextDocument((e) => {
+      if (
+        ameba &&
+        ameba.config.trigger === LintTrigger.Type &&
+        isValidCrystalDocument(e.document)
+      ) {
+        outputChannel.appendLine(
+          `[Change] Running ameba on ${getRelativePath(e.document)}`
+        );
+        ameba.execute(e.document, isDocumentVirtual(e.document));
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        workspace.onDidSaveTextDocument(doc => {
-            if (ameba && ameba.config.trigger === LintTrigger.Save && isValidCrystalDocument(doc)) {
-                outputChannel.appendLine(`[Save] Running ameba on ${getRelativePath(doc)}`)
-                ameba.execute(doc);
-            } else if (ameba && ameba.config.trigger !== LintTrigger.None && path.basename(doc.fileName) == ".ameba.yml") {
-                outputChannel.appendLine(`[Config] Reloading diagnostics after config file change`)
-                ameba.clear();
-                executeAmebaOnWorkspace(ameba);
-            }
-        })
-    );
+  context.subscriptions.push(
+    workspace.onDidSaveTextDocument((doc) => {
+      if (
+        ameba &&
+        ameba.config.trigger === LintTrigger.Save &&
+        isValidCrystalDocument(doc)
+      ) {
+        outputChannel.appendLine(
+          `[Save] Running ameba on ${getRelativePath(doc)}`
+        );
+        ameba.execute(doc);
+      } else if (
+        ameba &&
+        ameba.config.trigger !== LintTrigger.None &&
+        path.basename(doc.fileName) == '.ameba.yml'
+      ) {
+        outputChannel.appendLine(
+          `[Config] Reloading diagnostics after config file change`
+        );
+        ameba.clear();
+        executeAmebaOnWorkspace(ameba);
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        workspace.onDidCloseTextDocument(doc => {
-            if (!ameba || !isValidCrystalDocument(doc)) return;
-            let shouldClear = true;
-        
-            if (workspace.workspaceFolders) {
-                shouldClear = !workspace.getWorkspaceFolder(doc.uri);
-            }
-        
-            if (shouldClear) {
-                outputChannel.appendLine(`[Clear] Clearing ${getRelativePath(doc)}`)
-                ameba.clear(doc.uri);
-            }
-        })
-    );
+  context.subscriptions.push(
+    workspace.onDidCloseTextDocument((doc) => {
+      if (!ameba || !isValidCrystalDocument(doc)) return;
+      let shouldClear = true;
 
-    context.subscriptions.push(
-        workspace.onDidDeleteFiles(e => {
-            if (!ameba) return;
-        
-            for (const file of e.files) {
-                outputChannel.appendLine(`[Delete] Clearing ${file.fsPath}`)
-                ameba.clear(file)
-            }
-        })
-    );
+      if (workspace.workspaceFolders) {
+        shouldClear = !workspace.getWorkspaceFolder(doc.uri);
+      }
+
+      if (shouldClear) {
+        outputChannel.appendLine(`[Clear] Clearing ${getRelativePath(doc)}`);
+        ameba.clear(doc.uri);
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    workspace.onDidDeleteFiles((e) => {
+      if (!ameba) return;
+
+      for (const file of e.files) {
+        outputChannel.appendLine(`[Delete] Clearing ${file.fsPath}`);
+        ameba.clear(file);
+      }
+    })
+  );
 }
 
-export function deactivate() { }
+export function deactivate() {}
 
 function executeAmebaOnWorkspace(ameba: Ameba | null) {
-    if (!ameba || ameba.config.trigger === LintTrigger.None) return;
+  if (!ameba || ameba.config.trigger === LintTrigger.None) return;
 
-    if (ameba.config.scope === LintScope.File) {
-        for (const doc of workspace.textDocuments) {
-            if (isValidCrystalDocument(doc)) {
-                if (isDocumentVirtual(doc)) {
-                    if (ameba.config.trigger === LintTrigger.Type) {
-                        outputChannel.appendLine(`[Workspace] Running ameba on ${getRelativePath(doc)}`);
-                        ameba.execute(doc, true);
-                    }
-                } else {
-                    outputChannel.appendLine(`[Workspace] Running ameba on ${getRelativePath(doc)}`);
-                    ameba.execute(doc);
-                }
-            }
-        };
-    } else if (workspace.workspaceFolders) {
-        for (const folder of workspace.workspaceFolders) {
-            outputChannel.appendLine(`[Workspace] Running ameba on ${folder.name}`);
-            ameba.execute(folder);
+  if (ameba.config.scope === LintScope.File) {
+    for (const doc of workspace.textDocuments) {
+      if (isValidCrystalDocument(doc)) {
+        if (isDocumentVirtual(doc)) {
+          if (ameba.config.trigger === LintTrigger.Type) {
+            outputChannel.appendLine(
+              `[Workspace] Running ameba on ${getRelativePath(doc)}`
+            );
+            ameba.execute(doc, true);
+          }
+        } else {
+          outputChannel.appendLine(
+            `[Workspace] Running ameba on ${getRelativePath(doc)}`
+          );
+          ameba.execute(doc);
         }
+      }
     }
+  } else if (workspace.workspaceFolders) {
+    for (const folder of workspace.workspaceFolders) {
+      outputChannel.appendLine(`[Workspace] Running ameba on ${folder.name}`);
+      ameba.execute(folder);
+    }
+  }
 }
 
 function getRelativePath(document: TextDocument): string {
-    if (document.uri.scheme === 'untitled') {
-        return document.fileName
-    }
+  if (document.uri.scheme === 'untitled') {
+    return document.fileName;
+  }
 
-    const space: WorkspaceFolder =
-        workspace.getWorkspaceFolder(document.uri) ?? noWorkspaceFolder(document.uri)
-    return path.relative(space.uri.fsPath, document.uri.fsPath)
+  const space: WorkspaceFolder =
+    workspace.getWorkspaceFolder(document.uri) ??
+    noWorkspaceFolder(document.uri);
+  return path.relative(space.uri.fsPath, document.uri.fsPath);
 }
 
 export function noWorkspaceFolder(uri: Uri): WorkspaceFolder {
-    const firstWorkspaceFolder = workspace.workspaceFolders?.[0];
-    if (uri.scheme === 'untitled' && firstWorkspaceFolder) {
-        return firstWorkspaceFolder
-    }
+  const firstWorkspaceFolder = workspace.workspaceFolders?.[0];
+  if (uri.scheme === 'untitled' && firstWorkspaceFolder) {
+    return firstWorkspaceFolder;
+  }
 
-    return {
-        uri: Uri.file(path.dirname(uri.fsPath)),
-        name: path.basename(path.dirname(uri.fsPath)),
-        index: -1
-    }
+  return {
+    uri: Uri.file(path.dirname(uri.fsPath)),
+    name: path.basename(path.dirname(uri.fsPath)),
+    index: -1,
+  };
 }
 
 export function isValidCrystalDocument(doc: TextDocument): boolean {
-    return ['crystal', 'html.ecr'].includes(doc.languageId) &&
-        ['file', 'untitled'].includes(doc.uri.scheme)
+  return (
+    ['crystal', 'html.ecr'].includes(doc.languageId) &&
+    ['file', 'untitled'].includes(doc.uri.scheme)
+  );
 }
 
 export function isDocumentVirtual(document: TextDocument): boolean {
-    return document.isDirty || document.isUntitled || document.uri.scheme === 'untitled'
+  return (
+    document.isDirty ||
+    document.isUntitled ||
+    document.uri.scheme === 'untitled'
+  );
 }
--- src/taskQueue.ts
+++ src/taskQueue.ts
@@ -2,38 +2,38 @@ import { CancellationToken, CancellationTokenSource, Uri } from 'vscode';
 
 import { outputChannel } from './extension';
 
-
 /**
  * Task with async operation. It will be enqueued to and managed by
  * TaskQueue. Useful for spawning ChildProcess.
  */
 export class Task {
-    public readonly uri: Uri;
-    public isEnqueued: boolean = false;
-    private body: (token: CancellationToken) => Promise<void>;
-    private cancelTokenSource: CancellationTokenSource = new CancellationTokenSource();
-    private cancelToken: CancellationToken = this.cancelTokenSource.token;
+  public readonly uri: Uri;
+  public isEnqueued: boolean = false;
+  private body: (token: CancellationToken) => Promise<void>;
+  private cancelTokenSource: CancellationTokenSource =
+    new CancellationTokenSource();
+  private cancelToken: CancellationToken = this.cancelTokenSource.token;
 
-    /**
-     * @param body Function of task body, which returns callback called
-     *             when cancelation is requested. You should call
-     *             token.finished() after async operation is done.
-     */
-    constructor(uri: Uri, body: (token: CancellationToken) => Promise<void>) {
-        this.uri = uri;
-        this.body = body;
-    }
+  /**
+   * @param body Function of task body, which returns callback called
+   *             when cancelation is requested. You should call
+   *             token.finished() after async operation is done.
+   */
+  constructor(uri: Uri, body: (token: CancellationToken) => Promise<void>) {
+    this.uri = uri;
+    this.body = body;
+  }
 
-    public async run(): Promise<void> {
-        if (this.cancelToken.isCancellationRequested) return Promise.resolve();
+  public async run(): Promise<void> {
+    if (this.cancelToken.isCancellationRequested) return Promise.resolve();
 
-        const task = this;
-        return await task.body(this.cancelToken);
-    }
+    const task = this;
+    return await task.body(this.cancelToken);
+  }
 
-    public cancel(): void {
-        this.cancelTokenSource.cancel()
-    }
+  public cancel(): void {
+    this.cancelTokenSource.cancel();
+  }
 }
 
 /**
@@ -42,58 +42,59 @@ export class Task {
  * processes to prevent from running out machine resource.
  */
 export class TaskQueue {
-    private tasks: Task[] = [];
-    private busy: boolean = false;
+  private tasks: Task[] = [];
+  private busy: boolean = false;
 
-    public get length(): number {
-        return this.tasks.length;
+  public get length(): number {
+    return this.tasks.length;
+  }
+
+  public enqueue(task: Task): void {
+    if (task.isEnqueued)
+      throw new Error(`Task is already enqueued (uri: ${task.uri})`);
+
+    this.cancel(task.uri);
+    task.isEnqueued = true;
+    this.tasks.push(task);
+    this.kick();
+  }
+
+  public cancel(uri: Uri): void {
+    const uriString = uri.toString(true);
+
+    for (const task of this.tasks) {
+      if (task.uri.toString(true) === uriString) {
+        task.cancel();
+      }
     }
+  }
 
-    public enqueue(task: Task): void {
-        if (task.isEnqueued) throw new Error(`Task is already enqueued (uri: ${task.uri})`);
-
-        this.cancel(task.uri);
-        task.isEnqueued = true;
-        this.tasks.push(task);
-        this.kick();
+  public clear(): void {
+    for (const task of this.tasks) {
+      task.cancel();
     }
+  }
 
-    public cancel(uri: Uri): void {
-        const uriString = uri.toString(true);
+  private async kick(): Promise<void> {
+    if (this.busy) return;
+    this.busy = true;
 
-        for (const task of this.tasks) {
-            if (task.uri.toString(true) === uriString) {
-                task.cancel();
-            }
-        }
+    while (true) {
+      let task: Task | undefined = this.tasks[0];
+      outputChannel.appendLine(`[Task] ${this.tasks.length} tasks in queue`);
+
+      if (!task) {
+        this.busy = false;
+        return;
+      }
+
+      try {
+        await task.run();
+      } catch (err) {
+        console.error('Error while running ameba:', err);
+      } finally {
+        this.tasks.shift();
+      }
     }
-
-    public clear(): void {
-        for (const task of this.tasks) {
-            task.cancel();
-        }
-    }
-
-    private async kick(): Promise<void> {
-        if (this.busy) return;
-        this.busy = true;
-
-        while (true) {
-            let task: Task | undefined = this.tasks[0];
-            outputChannel.appendLine(`[Task] ${this.tasks.length} tasks in queue`);
-
-            if (!task) {
-                this.busy = false;
-                return;
-            }
-
-            try {
-                await task.run();
-            } catch (err) {
-                console.error('Error while running ameba:', err);
-            } finally {
-                this.tasks.shift();
-            }
-        }
-    }
+  }
 }
