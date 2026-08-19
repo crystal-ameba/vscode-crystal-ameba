94134e1c345676551b03443c69d5b827e39f8dda
Sijawusz Pur Rahnama <sija@sija.pl>
2026-08-19 10:52:57 +0200

    Make ESLint ignore entries from `.gitignore`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'eslint/config';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import { fileURLToPath } from 'node:url';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
+
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, { gitignoreResolution: true }),
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
